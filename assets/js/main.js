b76dc401a4e24b88a543b0a99b7632fe9cd8f3f5
goslav <bartoszgoslawski00@gmail.com>
2026-01-19 14:18:23 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
+const slides = document.querySelectorAll('.slides img');
+let index = 0;
 
+function showSlide(i) {
+  slides.forEach(slide => slide.classList.remove('active'));
+  slides[i].classList.add('active');
+}
+
+document.querySelector('.next')?.addEventListener('click', () => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+});
+
+document.querySelector('.prev')?.addEventListener('click', () => {
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+});
+
+setInterval(() => {
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}, 4000);
